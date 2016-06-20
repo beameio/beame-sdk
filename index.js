7650f74e659026d31376c8476684188bc5a55e1e
zglozman <z@beame.io>
2016-06-20 13:39:57 -0400

    added_files

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	devCreate: require("./src/devCreate.js"), //  <developerName>
+/*	devGetCert:require("./src/devGetCert.js"), // <developer hostname>
+	devProfileUpdate: require("./src/devProfileUpdate.js"),// <developer hostname>
+	devAppSave: require("./src/devAppSave.js"),// <developer hostname> <appName>
+	devAppGetCert: require("./src/devAppGetCert.js"),// <developer hostname> <app hostname>
+	devAppUpdate: require("./src/devAppUpdate.js"),// <developer hostname> <app hostname> <newAppName>
+	edgeClientRegister: require("./src/edgeClientRegister.js"),// <developer hostname> <app hostname> 
+	edgeClientGetCert: require("./src/edgeClientGetCert.js") // <developer hostname> <app hostname> <client hostname>*/
+}
---
+++ src/devCreate.js
@@ -0,0 +1,101 @@
+//var test=require('../beame-provision-test/main.js');
+var test=require('beame-provision-test');
+var fs=require('fs');
+var keys = ["$id","hostname","uid"];    //expected answer keys
+var devPath = "./.beame/";              //path to store dev data: uid, hostname, key, certs, appData
+
+try{
+	if(!fs.lstatSync(devPath)){
+		console.log("Dev path ", devPath);
+		fs.mkdirSync(devPath);
+	}
+}
+catch(e) { 
+	console.log("Directory for developer already exists");
+}
+
+/* Expected answer (values gonna change):
+ *  "$id": "1",
+ *  "hostname": "lkdz51o29q1hlfmusixn3ryilfhm5vdi.v1.beameio.net",
+ *  "uid": "96fc1f42-30ac-4829-bbcc-3f518fcefcb5"
+ * 
+ */
+
+/*console.log(process.argv);
+if (process.argv.length < 3) {
+    console.log('Usage: node '+__filename+' nikname');
+    process.exit(-1);
+}*/
+
+var param=process.argv[2];              //get name from cli
+console.log('Running api with cli param: '+param);
+
+var authData={
+    pk:"./authData/pk.pem",
+    x509:"./authData/x509.pem",
+    generateKeys:false,
+    makeCSR:false,
+    CSRsubj:"/C=US/ST=Florida/L=Gainesville/O=LFE.COM, Inc/OU=Development/CN=doesntMatter"
+};
+
+var createDeveloperRequest = function(param, callback){
+    
+	postData={
+        name:param
+    };
+    
+	var testParams={
+        version:"/v1",
+        api:"/dev/create",
+        postData:postData,
+        answerExpected:true,
+        decode:false
+    };
+    
+	test.runRestfulAPI(testParams,function(err,payload){
+        if(!err){
+            var i;
+            var devDir=devPath+payload.hostname+'/';
+            if (!fs.existsSync(devDir)){
+                fs.mkdirSync(devDir);//create directory for new developer, named with his unique hostname
+            }
+            for(i=0;i<keys.length;i++){
+                if(payload[keys[i]] !== undefined){
+                    console.log(keys[i] + ': ' + payload[keys[i]]);
+					// next is single test use only,
+					// eventually, this gonna create folder for each user to be re-used in following multi-user tests:
+                    fs.writeFile(devDir+keys[i],payload[keys[i]]);
+                }
+                else{
+                    console.log('Error, missing <' + keys[i] + '> element in provisioning answer');
+                    process.exit(-1);
+                }
+            }
+			if(callback){
+				 callback(payload);
+			}
+        }
+        else{
+			console.log('Fail: '+err);
+		}
+
+    });
+};
+ 
+
+var requestCreateDeveloper = function(developerName, callback){
+	var devName = developerName;
+
+	test.setAuthData(authData, function(csr, pk){
+		
+		createDeveloperRequest(developerName);
+		//callback will return null,null so
+		//nothing special to do here, this is 
+		//to use in further activities: update / getCert etc
+
+	} );
+};
+
+	
+module.exports =  requestCreateDeveloper;
+
