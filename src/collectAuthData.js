16ecdefaaa7971f6b57bf0bd92e342652306f82e
zglozman <z@beame.io>
2016-06-22 12:16:10 -0400

    lots of changes

--- src/collectAuthData.js
+++ src/collectAuthData.js
@@ -1,160 +1,103 @@
+'use strict';
 var fs = require('fs');
+var path = require('path');
 var dataCollection;
 var jsonElement;
+var _=require('underscore');
 var os = require('os');
 var usrFiles = ["uid","hostname","x509","ca","private_key.pem","pkcs7","name"];
 var pathDepths=0;//0=~/.beame 1=developer 2=app 3=client
-module.exports.authDataCollection = function(inPath, callback){
-    var devPath = os.homedir()+"/.beame/";//replace with inPath
-    var i_dev, i_app, i_client;
 
-    var q = "\"";
-    var qs= "\":\"";
-    var qc="\",";
-    var getAuthData = function(dirPath, key){
-        var ret="";
-        var str = "";
-        str = fs.readFileSync(dirPath + "hostname");
-        ret += q+key[0]+qs+str+qc;
-        str = fs.readFileSync(dirPath + "x509");
-        ret += q+key[1]+qs+str+qc;
-        str = fs.readFileSync(dirPath + "private_key.pem");
-        ret += q+key[2]+qs+str+qc;
-        return ret;
-    }
+//
+//levels: developer, app instance
+//
+//
+var levels = ['developer', 'app', 'developer'];
 
 
-    fs.readFile(devPath + 'developers',function(err,data){
-        if(!err){
-            dataCollection += "[";//initialize top level of json array
-     /* -------- loop through developers -----------*/
-            var developers = data.toString().split('\r\n'); 
-            for(i_dev=0;i_dev<developers.length;){
-               if(developers[i_dev].length < 25){
-                   i_dev++;
-                   continue;
-               } 
-                
-                var devIsOk = true;
-                var devDir = devPath + developers[i_dev] + "/";
-     /* -------- verify developer -----------------*/
-               console.log('Reading developer: '+devDir); 
-               for(i=0;i<usrFiles.length;i++){
-                    if(!fs.existsSync(devDir+usrFiles[i])){
-                        console.log('Error! missing: '+devDir+usrFiles[i]);
-                        //developer not properly defined
-                        devIsOk = false;
-                    }
-                }
 
-                if(devIsOk){
-                    var devName = fs.readFileSync(devDir + "name");
-                    dataCollection += "{\"devname\":\""  + devName+qc;
-                    dataCollection += "{"+getAuthData(devDir, ["hostname","devCert","devKey"]);
-                    fs.readFile(devDir + 'apps',function(err,data){
-                        if(!err){
-                            dataCollection += "\"apps\":[";//initialize app level in json array
-     /* -------- loop through apps -----------------*/
-                            var apps = data.toString().split('\r\n');
-                            for(i_app=0;i_app<apps.length;){
+var getNextLevel = function(level){
+	switch(level){
+		case "developer":
+			return 0 + 1;
+		case "app":
+			return 1 + 1;
+		case "instance":
+			return -1;
+		default:
+			return -1;
+	}
+};
 
-     /* -------- verify app -----------------------*/
-                                if(apps[i_app].length < 25){
-                                    i_app++;
-                                    continue;
-                                }
-                                var appDir = devDir + apps[i_app]+"/";
-                                console.log('reading auth data for app::'+appDir);
-                                var appIsOk = true;
+var keyPair = function(sourceDir, level, allDone){
+	this.credentials = {
+		"name":fs.readFileSync(sourceDir + "name"),
+		"key": fs.readFileSync(sourceDir + "private_key.pem"),
+		"cert": fs.readFileSync(sourceDir + "x509"),
+		"hostname": fs.readFileSync(sourceDir + "hostname")
+	};
+	var nextLevel = getNextLevel(level);
 
-                                for(i=0;i<usrFiles.length;i++){
-                                    if(!fs.existsSync(appDir+usrFiles[i])){
-                                        console.log('Error! missing: '+appDir+usrFiles[i]);
-                        //app is not properly defined
-                                        appIsOk = false;
-                                    }
-                                }
-                                if(appIsOk){
-                                    var appName = fs.readFileSync(appDir + "name");
-                                    dataCollection += "{\"appname\":\""  + appName+qc;
-                                    dataCollection += getAuthData(appDir, ["hostname","appCert","appKey"]);
+	dataLogger.getDependantsSync(sourceDir, nextLevel, allDone);/*function(tree){
 
-                                    fs.readFile(appDir+'edgeClients', function(err,data){
-                                        if(!err){
-                                            dataCollection += "\"instances\":[";//initialize hosts level in json array
-                                        
-                        /* -------- loop through clients -----------------*/
-                                            var clients = data.toString().split('\r\n');
-                                            for(i_client=0;i_client<clients.length;){
+		allDone && allDone(this.credentials.name, this.credentials);
+	
+	});*/
+};
 
-                        /* -------- verify client -----------------------*/
-                                                if(clients[i_client].length < 25){
-                                                    i_client++;
-                                                    continue;
-                                                }
-                                                var clientDir = appDir + clients[i_client] + "/";
-                                                var clientIsOk = true;
-                                    console.log('reading auth data for instance::'+appDir);
-                                                for(i=0;i<usrFiles.length - 1;i++){//- "name"
-                                                    if(!fs.existsSync(clientDir+usrFiles[i])){
-                                                        console.log('Error! missing: '+clientDir+usrFiles[i]);
-                                    //app is not properly defined
-                                                        clientIsOk = false;
-                                                    }
-                                                }
-                                                if(clientIsOk){
-                                                    dataCollection += "{"+getAuthData(clientDir, ["hostname","cert","key"]);
-                                                }
-                                                else{
-                                                    dataCollection += "{";//empty instance
-                                                }
-                                                if(++i_client<clients.length){
-                                                    dataCollection += "},";
-                                                }
-                                                else{
-                                                    dataCollection += "}";
-                                                }
-                                            }//loop thru clients
-                                        }
-                                    });
-                                }//if app is ok
-                                else{
-                                    console.log('app in NOK');
-                                    dataCollection += "{}"; 
-                                }
-            /* ----------- close apps element ---------------------*/
-                                if(++i_app < apps.length){
-                                    dataCollection += "},";
-                                }
-                                else{
-                                    dataCollection += "}";
-                                }
-                            }//loop thru apps
-                            dataCollection += "]";//close app array
-                        }//if apps file exists in developer
-                        else{
-                            dataCollection += "\"apps\":\"\"";//no apps defined for developer
-                        }
-                    });
-                }
-                else{
-                    console.log('developer is NOK');
-                }
-
-            /* ----------- close developers element ---------------*/
-                if(++i_dev < developers.length){
-                    dataCollection += "},";
-                }
-                else{
-                    dataCollection += "}";
-                }
-            }//loop thru developers
-            dataCollection+="]";//close developers array
-        }
-        else{
-            console.log('no developers in:' + devPath);
-            dataCollection+="{}";//no developers defiled!
-        }
-        callback(dataCollection);
-    });
+function scanBeameDir(beameDir){
+	console.log('start');
+	if(beameDir.length === 0){
+		beameDir = os.homedir() + "/.beame/";
+	}
+	var developers = [];
+	fs.readdirSync(beameDir, _.bind(function(err, data){
+		console.log('data'+data);	
+		_.each(data, function(item){
+			var stat = fs.statSync(item);
+			console.log('scanBeameDir: '+item);
+			if (stat && stat.isDirectory()) {
+				var dataLogger = new keyPair(item, levels[0],  function(tree){ 
+					console.log(JSON.stringify(tree));
+				});
+			}
+		});
+	}), this);
 }
+
+keyPair.prototype.getDependantsSync = function(currentDir, level, done ){
+	fs.readdirSync(currentDir, _.bind(function(err, data){
+		if (err) {  return done(err);	}
+		_.each(data, _.bind(function(item){
+			var stat = fs.statSync(item);
+			if (stat && stat.isDirectory()) {
+				var newKeyPair = new keyPair(item, level);
+
+				switch(level){
+					case "developer":
+					
+						if(!this.credentials.apps)
+						{
+							this.credentials.apps = [];
+						}
+				
+						this.credentials.apps.push(newKeyPair.credentials);
+					break;
+					case "app":
+						if(!this.credentials.instances)
+						{
+							this.credentials.instances = [];
+						}
+				
+						this.credentials.instances.push(newKeyPair.credentials);
+					break;
+					default:
+						done(this.credentials);
+				}
+			}
+		}), this);
+		
+	}, this));
+};
+
+scanBeameDir(os.homedir()+'/.beame/');
