08b741162132e49e161f05791dd44f46beb85d13
zglozman <z@beame.io>
2016-06-23 13:16:30 -0400

    s

---
+++ tests/httsServerTest.js
@@ -0,0 +1,12 @@
+var https = require("https");
+var proxyClient = require("../src/proxyClient.js");
+var beameapi = require("../index.js");
+
+var server = function(){
+	beameapi.scanBeameDir(function(){
+		
+		console.log(object);
+	});
+}
+
+
