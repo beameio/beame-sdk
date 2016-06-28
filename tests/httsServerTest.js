fe868588d9173e6b530531b0e707f7718e7b10fb
zglozman <z@beame.io>
2016-06-28 15:43:47 -0400

    first working server demo

--- tests/httsServerTest.js
+++ tests/httsServerTest.js
@@ -1,12 +1,32 @@
 var https = require("https");
-var proxyClient = require("../src/proxyClient.js");
+var ProxyClient = require("beame-proxy-clients");
 var beameapi = require("../index.js");
+var beame_utils = require("beame-utils");
 
-var server = function(){
-	beameapi.scanBeameDir(function(){
-		
-		console.log(object);
-	});
+var Server = function(){
+	beameapi.scanBeameDir.scanBeameDir("", function(object){
+		var instance = object[0].chain.apps[0].instances[0];
+		var outInstanceName = instance.hostname + "";
+		var proxyUtils = new beame_utils.ProxyUtils();
+		proxyUtils.selectBestProxy(undefined, function (err, data) {
+			console.log(data);
+			const options = {
+				key: instance.key,
+				cert: instance.cert
+			};
+
+			https.createServer(options, function(req, res){
+				res.writeHead(200);
+				res.end('hello world\n');
+			}).listen(8000, function() {
+                //function ProxyClient(serverType, edgeClientHostname, edgeServerHostname, targetHost, targetPort, options, agent, edgeClientCerts) {
+				var proxy = new ProxyClient.ProxyClient("HTTPS", outInstanceName, data.endpoint, 'localhost', 8000, undefined, undefined, options);
+                console.log("Registered Instance : " + outInstanceName);
+
+			});
+        });
+    })
 }
 
 
+server = new Server();
