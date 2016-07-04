8a299dee5dc8738ae16f2a88e451b2c41fb20f74
Ilya Sher <ilya.sher@coding-knight.com>
2016-07-04 16:35:17 +0300

    cleanup

--- tests/testHttpsClient.js
+++ tests/testHttpsClient.js
@@ -4,28 +4,25 @@ var io = require('socket.io-client');
 
 console.log("Argv " + argv.instance);
 
-  var socket = io.connect( argv.instance,{secure: true});
-  
-  socket.on('connect', function(data){
+var socket = io.connect(argv.instance, {secure: true});
+
+socket.on('connect', function(data){
 	console.log("Connected " + data);
-  });
-  var counter = 0;
-  var timeStart = 	new Date().getTime();
-  
-  socket.on('iping', function(data){
-	
-  	 	if(new Date().getTime() - timeStart > 1000){
-			console.log("counter is " + counter);
-			counter=0;
+});
+var counter = 0;
+var timeStart = new Date().getTime();
 
-   			timeStart = 	new Date().getTime();
-		}
-		counter++;
-	  	socket.emit('ipong', { pong: "ipong" } );
-  });
-  
-  
-  socket.on('disconnect', function(){
+socket.on('iping', function(data){
+	if(new Date().getTime() - timeStart > 1000){
+		console.log("counter is " + counter);
+		counter = 0;
+		timeStart = new Date().getTime();
+	}
+	counter++;
+	socket.emit('ipong', { pong: "ipong" } );
+});
+
+
+socket.on('disconnect', function(){
 	console.log("Disconnected ");
-  });
-
+});
