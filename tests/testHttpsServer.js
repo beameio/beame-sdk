29d6552300b40035d4afe3518f476ff8b84dd5f4
Ilya Sher <ilya.sher@coding-knight.com>
2016-07-04 16:30:44 +0300

    cleanup

--- tests/testHttpsServer.js
+++ tests/testHttpsServer.js
@@ -7,7 +7,6 @@ var io = new Server();
 var argv = require('minimist')(process.argv.slice(2));
 var _ = require('underscore');
 var fs = require('fs');
-//console.log(argv);
 
 var targetDeveloper = argv.developer;
 
@@ -18,23 +17,21 @@ var Server = function(){
 			return (item.chain.hostname + "") == targetDeveloper;
 			
 		});
-		if(!developer || developer === ""){
-			console.error("Developer " + targetDeveloper + " is not found in the");
+		if(!developer){
+			console.error("Developer " + targetDeveloper + " is not found.");
 			process.exit(1);
 		}
-			
+
 		var instance = developer.chain.apps[0].instances[0];
 		var outInstanceName = instance.hostname + "";
-		var edgeHostname = instance.edgeHostname;
-		var proxyUtils = new beame_utils.ProxyUtils();
-        
+
 		var options = {
 			key: instance.key,
 			cert: instance.cert
 		};
 		console.log(options.cert);
 		var app = https.createServer(options, function(req, res){
-			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.writeHead(200, {'Content-Type': 'text/plain', 'Server': 'Beame.io test server'});
 			res.end('hello world\n');
 			console.log("returning hello world");
 		});
@@ -48,15 +45,10 @@ var Server = function(){
 			});
 		});
 		app.listen(8000, function() {
-			//function ProxyClient(serverType, edgeClientHostname, edgeServerHostname, targetHost, targetPort, options, agent, edgeClientCerts) {
 			var proxy = new ProxyClient("HTTPS", outInstanceName, instance.edgeHostname, 'localhost', 8000, undefined, undefined, options);
 			console.log("Registered Instance : " + outInstanceName);
-
-
-
 		});
     })
 }
-
 
 var server = new Server();
