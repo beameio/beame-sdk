0344ede54d5d67fa41158b1b6403331601e00d74
Ilya Sher <ilya.sher@coding-knight.com>
2016-07-05 12:16:17 +0300

    WIP: CLI

--- cli/beame.js
+++ cli/beame.js
@@ -2,44 +2,45 @@
 var argv = require('minimist')(process.argv.slice(2));
 var _ = require('underscore');
 
-var commands = { 
-	"creds": { "file": "./creds.js" } ,
-	"certs": { "file": "./certs.js" },
-	"data": { "file": "./data.js" }
-};
+var commands = {}
+_.map(['creds', 'certs', 'data'], function(module) {
+	commands[module] = require('./' + module + '.js')
+});
 
-function argumentNames(fun) {
+function getParamsNames(fun) {
 	var names = fun.toString().match(/^[\s\(]*function[^(]*\(([^)]*)\)/)[1]
 		.replace(/\/\/.*?[\r\n]|\/\*(?:.|[\r\n])*?\*\//g, '')
 		.replace(/\s+/g, '').split(',');
 	return names.length == 1 && !names[0] ? [] : names;
 }
 
-function command () {
-	var selectedCommand = argv._[0];
-	var subCommand = argv._[1];
+function main() {
+	var cmdName = argv._[0];
+	var subCmdName = argv._[1];
 
-	var foundCommand = commands[selectedCommand];
+	var cmd = commands[cmdName];
 
-	if(foundCommand){
-		try {
-			var module = require(foundCommand.file);
-			if(module) {
-				if(typeof module[subCommand] === 'function') {
-					var acceptedArgs = argumentNames(module[subCommand]);
-					var argumentsArray = [];
-
-					_.each(acceptedArgs, function(item){
-						argumentsArray.push(argv[item]);
-					});
-					module[subCommand].apply(null, argumentsArray);
-				}
-			}
-		} catch(e) {
-			console.error("Error module not found $j", e.toString());
-		}
+	if(!cmd) {
+		throw new Error("Command '"+cmdName+"' not found. Valid top-level commands are: " + _.keys(commands));
 	}
-	//console.log(foundCommand);
+
+	if(typeof commands[cmdName][subCmdName] !== 'function') {
+		throw new Error("Sub-command '"+subCmdName+"' for command '"+cmdName+"' not found. Valid sub-commands are: " + _.keys(commands[cmdName]));
+	}
+
+	var paramsNames = getParamsNames(commands[cmdName][subCmdName]);
+	var args = _.map(paramsNames, function(paramName) { return argv[paramName]; });
+	commands[cmdName][subCmdName].apply(null, args);
 }
 
-command();
+function usage() {
+    console.log("Usage:");
+    console.log("  TODO");
+}
+
+if(argv._.length < 2) {
+    usage();
+    process.exit(1);
+}
+
+main();
