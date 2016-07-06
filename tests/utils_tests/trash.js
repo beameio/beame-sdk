f64d1d0a698a39c9ed633589a264a29b14facd10
zenit1 <serge@beame.io>
2016-07-06 10:52:42 +0300

    sys removed

--- tests/utils_tests/trash.js
+++ tests/utils_tests/trash.js
@@ -4,7 +4,7 @@
 var path = require('path');
 var os = require('os');
 var home = os.homedir();
-var devPath = path.join(home ,".beame");              //path to store dev data: uid, hostname, key, certs, appData
+var devPath = path.join(home ,".beame/");              //path to store dev data: uid, hostname, key, certs, appData
 var __homedir = home;
 
 console.log('dev path is ', devPath);
