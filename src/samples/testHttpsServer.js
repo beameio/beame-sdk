688ed7a5ff29784ad0f696eb562412f64f8c9b74
zglozman <z@beame.io>
2016-07-09 14:53:48 -0400

    final changes to sampeleHttpServer

--- src/samples/testHttpsServer.js
+++ src/samples/testHttpsServer.js
@@ -1,44 +1,39 @@
 var https = require("https");
 var ProxyClient = require("beame-ssl-proxy-client");
-var beamestore = require("../services/BeameStore");
+var BeameStore = require("../services/BeameStore");
 
 var _ = require('underscore');
 var fs = require('fs');
 
 var beameDirServices = require('../services/BeameDirServices');
 var debug = require("debug")("SampleBeameServer");
-
+var beamestore = new BeameStore();
 var SampleBeameServer = function(instanceHostname, hostOnlineCallback)
 {
     if(!instanceHostname) {
         throw new Error("No instance found");
     }
 
-				var edgeCert = beamestore.searchEdge(instanceHostname);
-				var options = {
-								key: edgeCreds.PRIVATE_KEY,
-								cert: edgeCreds.P7B,
-								ca: edgeCreds.CA
+    var edgeCert = beamestore.search(instanceHostname)[0];
+    var options = {
+        key: edgeCert.PRIVATE_KEY,
+        cert: edgeCert.P7B,
+        ca: edgeCert.CA
+    };
 
-								// Client certificates - start
-//										requestCert: true,
-//										rejectUnauthorized: false
-								// Client certificates - end
-				};
+    var app = https.createServer(options);
+    app.listen(0, function() {
+        //ProxyClient(serverType, edgeClientHostname, edgeServerHostname, targetHost, targetPort, options, agent, edgeClientCerts)
+        var onLocalServerCreated = function(data){
+            if(hostOnlineCallback){
+                hostOnlineCallback(data, app);
+            }
 
-								var app = https.createServer(options);
-								app.listen(0, function() {
-												//ProxyClient(serverType, edgeClientHostname, edgeServerHostname, targetHost, targetPort, options, agent, edgeClientCerts)
-												var onLocalServerCreated = function(data){
-																if(hostOnlineCallback){
-																				hostOnlineCallback(data, app);
-																}
+        };
 
-												};
-												
-												address = app.address();
-												var proxy =new ProxyClient("HTTPS", creds.hostname, creds.edgeHostname, 'localhost', app.address().port, {"onLocalServerCreated": onLocalServerCreated } , undefined, options);
-												});
+        address = app.address();
+        var proxy =new ProxyClient("HTTPS", edgeCert.hostname, edgeCert.edgeHostname, 'localhost', app.address().port, {"onLocalServerCreated": onLocalServerCreated } , undefined, options);
+    });
 };
 
 module.exports = { "SampleBeameServer":SampleBeameServer};
