0fe19168429a7bfeb41561e367075d36004aeba6
zenit1 <serge@beame.io>
2016-07-21 11:42:22 +0300

    stats added to creds cli

--- src/cli/creds.js
+++ src/cli/creds.js
@@ -1,66 +1,64 @@
 "use strict";
-
+//var JSON = require('JSON');
 var debug = require("debug")("cred_api");
 var Table = require('cli-table2');
 var x509 = require('x509');
 
 var store = new (require("../services/BeameStore"))();
 require('./../utils/Globals');
-var developerServices = new(require('../core/DeveloperServices'))();
-var atomServices = new(require('../core/AtomServices'))();
-var edgeClientServices = new(require('../core/EdgeClientServices'))();
+var developerServices = new (require('../core/DeveloperServices'))();
+var atomServices = new (require('../core/AtomServices'))();
+var edgeClientServices = new (require('../core/EdgeClientServices'))();
 
-var beameDirService = require('../services/BeameDirServices');
 var path = require('path');
 var fs = require('fs');
-var apiConfig = require("../../config/ApiConfig.json");
 var mkdirp = require("mkdirp");
 
-function listCreds(type, fqdn){
+function listCreds(type, fqdn) {
 	var returnValues = [];
-	if(type && !fqdn) {
+	if (type && !fqdn) {
 		returnValues = store.list(type);
 	}
-	if(!type && fqdn) {
+	if (!type && fqdn) {
 		returnValues = store.list("", fqdn);
 	}
-	if(!type && !fqdn) {
+	if (!type && !fqdn) {
 		returnValues = store.list();
 	}
 	return returnValues;
 }
 
-function show(type, fqdn){
-	debug("show %j %j", type,  fqdn);
+function show(type, fqdn) {
+	debug("show %j %j", type, fqdn);
 
 	var creds = listCreds(type, fqdn);
-	var certs = creds.map(cert => {
+	return creds.map(cert => {
 		var item = store.search(cert.hostname)[0];
 		return x509.parseCert(item.X509 + "");
 	});
 
-	return certs;
 }
 
-show.toText = function(certs) {
+show.toText = function (certs) {
 	var table = new Table({
 		head: ['Name', "Print", "Serial", "SigAlg"],
 		colWidths: [25, 65, 30, 30]
 	});
 
 	certs.forEach(xcert => {
-		table.push([xcert.subject.commonName, xcert.fingerPrint, xcert.serial,	xcert.signatureAlgorithm]);
+		//noinspection JSUnresolvedVariable
+		table.push([xcert.subject.commonName, xcert.fingerPrint, xcert.serial, xcert.signatureAlgorithm]);
 	});
 	return table;
 };
 
 
-function list(type, fqdn){
-	debug("list %j %j", type,  fqdn);
+function list(type, fqdn) {
+	debug("list %j %j", type, fqdn);
 	return listCreds(type, fqdn);
 }
 
-list.toText = function(creds) {
+list.toText = function (creds) {
 	var table = new Table({
 		head: ['name', 'hostname', 'level'],
 		colWidths: [15, 70, 15]
@@ -78,8 +76,8 @@ function lineToText(line) {
 }
 
 function _stdCallback(callback) {
-	return function(error, data) {
-		if(error) {
+	return function (error, data) {
+		if (error) {
 			console.error(error);
 			process.exit(1);
 		} else {
@@ -88,34 +86,34 @@ function _stdCallback(callback) {
 	};
 }
 
-function createTestDeveloper(developerName, developerEmail, callback){
+function createTestDeveloper(developerName, developerEmail, callback) {
 	debug("Creating test developer developerName=%j developerEmail=%j", developerName, developerEmail);
 	developerServices.createDeveloper(developerName, developerEmail, _stdCallback(callback));
 }
 createTestDeveloper.toText = lineToText;
 
-function createAtom(developerFqdn, atomName, howMany, callback){
-	if(!howMany){
-		howMany =1;
+function createAtom(developerFqdn, atomName, howMany, callback) {
+	if (!howMany) {
+		howMany = 1;
 	}
-	for(var i = 0; i < howMany; i++){
+	for (var i = 0; i < howMany; i++) {
 		console.warn("Creating atom developerFqdn=%j atomName=%j", developerFqdn, atomName);
-		atomServices.createAtom(developerFqdn, atomName+i, _stdCallback(callback));
+		atomServices.createAtom(developerFqdn, atomName + i, _stdCallback(callback));
 	}
 }
 createAtom.toText = lineToText;
 
-function createDeveloper(developerFqdn, uid, callback){
+function createDeveloper(developerFqdn, uid, callback) {
 	console.warn("Creating developer developerFqdn=%j uid=%j ", developerFqdn, uid);
-	developerServices.completeDeveloperRegistration(developerFqdn ,uid, _stdCallback(callback));
+	developerServices.completeDeveloperRegistration(developerFqdn, uid, _stdCallback(callback));
 }
 createDeveloper.toText = lineToText;
 
-function createEdgeClient(atomFqdn, howMany, callback ){
-	if(!howMany){
-		howMany =1;
+function createEdgeClient(atomFqdn, howMany, callback) {
+	if (!howMany) {
+		howMany = 1;
 	}
-	for(var i = 0; i < howMany; i++){
+	for (var i = 0; i < howMany; i++) {
 		console.warn("Creating edge client atomFqdn=%j", atomFqdn);
 		edgeClientServices.createEdgeClient(atomFqdn, _stdCallback(callback));
 	}
@@ -123,76 +121,79 @@ function createEdgeClient(atomFqdn, howMany, callback ){
 
 createEdgeClient.toText = lineToText;
 
-function constructRelateivePathElements(item){
+function constructRelateivePathElements(item) {
 	var items = [];
 	var upShot = item;
 	items.push(upShot.hostname);
-	while(upShot.parent_fqdn){
+	while (upShot.parent_fqdn) {
 		upShot = store.search(upShot.parent_fqdn)[0];
 		items.unshift(upShot.hostname);
 	}
 	return items;
 }
 
-function importNonBeameCredentials(fqdn){
-  var tls = require('tls');
-	var creds = store.search(fqdn)[0];
-	var conn = tls.connect(443, fqdn, {host: fqdn}, function() {
+function importNonBeameCredentials(fqdn) {
+	var tls = require('tls');
+	var conn = tls.connect(443, fqdn, {host: fqdn}, function () {
+		//noinspection JSUnresolvedFunction
 		var cert = conn.getPeerCertificate(true);
-		conn.end();		
+		conn.end();
 		var buffer = new Buffer(cert.raw, "hex");
 		var certBody = "-----BEGIN CERTIFICATE-----\r\n";
-		certBody+=buffer.toString("base64");
-		certBody+="-----END CERTIFICATE-----";
+		certBody += buffer.toString("base64");
+		certBody += "-----END CERTIFICATE-----";
 		var remoteCertPath = path.join(global.globalPath, 'v1', 'remote', fqdn, 'x509.pem');
-		var requestPath = apiConfig.Endpoints.CertEndpoint + '/' + fqdn + '/' + 'x509.pem';
+		//var requestPath = apiConfig.Endpoints.CertEndpoint + '/' + fqdn + '/' + 'x509.pem';
 
 		mkdirp(path.parse(remoteCertPath).dir);
 		fs.writeFileSync(remoteCertPath, certBody);
 	});
-};
+}
 
-function exportCredentials(fqdn, targetFqdn, file){
+function exportCredentials(fqdn, targetFqdn, file) {
 	var creds = store.search(fqdn)[0];
-	var relateivePath = constructRelateivePathElements(creds);
+	var relativePath = constructRelateivePathElements(creds);
 
 	creds.edgeclient = {};
 	creds.atom = {};
-	creds['relativePath'] = relateivePath;
+	creds['relativePath'] = relativePath;
 	creds.path = creds.path.replace(global.devPath, "");
 
 	var jsonString = JSON.stringify(creds);
-	if(!jsonString){
+	if (!jsonString) {
 		console.error("Credentials for exporting are not found");
 		return -1;
 	}
 	var crypto = require('./crypto');
 	var encryptedString;
-	try{
+	try {
 		encryptedString = crypto.encrypt(jsonString, targetFqdn);
-	} catch(e){ console.error("Cound not encrypt", e); return {}; } 
+	} catch (e) {
+		console.error("Cound not encrypt", e);
+		return {};
+	}
 
-	var message= {
-		signedData :{
-			data: encryptedString, 
+	var message = {
+		signedData: {
+			data: encryptedString,
 			signedby: fqdn,
 			encryptedFor: targetFqdn
 		}
 	};
-	message.signature = JSON.stringify(crypto.sign(message.signedData, fqdn ));
-	if(!file) {
+	message.signature = JSON.stringify(crypto.sign(message.signedData, fqdn));
+	if (!file) {
 
 	}
-	else{
+	else {
 		var p = path.resolve(file);
 		fs.writeFileSync(p, JSON.stringify(message));
 		return p;
 	}
 }
 
-function readStdinStream(callback){
+function readStdinStream(callback) {
 	var stdin = process.stdin,
-		stdout = process.stdout,
+		//stdout = process.stdout,
 		inputChunks = [];
 
 	stdin.resume();
@@ -207,101 +208,130 @@ function readStdinStream(callback){
 	});
 }
 
-function  decryptCreds(data) {
+function decryptCreds(data) {
 	var crypto = require('./crypto');
 	var parsedData = JSON.parse(data);
 
 	var signatureStatus = crypto.checkSignature(parsedData.signedData, parsedData.signedData.signedby, parsedData.signature);
-	if(signatureStatus === true) {
+	if (signatureStatus === true) {
 		var creds = store.search(parsedData.signedData.encryptedFor)[0];
 
-		if(!creds){
-			console.error("Private key for %j is not found", parsedData.signedData.encryptedFor)
+		if (!creds) {
+			console.error("Private key for %j is not found", parsedData.signedData.encryptedFor);
 			return -1;
 		}
-		var decryptedcreds = crypto.decrypt(JSON.stringify(parsedData.signedData.data));
-		return decryptedcreds;
+
+		return crypto.decrypt(JSON.stringify(parsedData.signedData.data));
 	}
 }
 
-function importCredentials(data, file){
+function importCredentials(data, file) {
 	var decryptedCreds;
 
-	if(!data && !file) {
+	if (!data && !file) {
 		// XXX: to test
 		readStdinStream(function (data) {
 			decryptedCreds = decryptCreds(data);
 			store.importCredentials(decryptedCreds);
 		});
-	}else {
-		if(file){
-			data = fs.readFileSync(path.resolve(file))+ "";
+	} else {
+		if (file) {
+			data = fs.readFileSync(path.resolve(file)) + "";
 			decryptedCreds = decryptCreds(data);
-			if(!decryptedCreds || decryptedCreds  == -1){
+			if (!decryptedCreds || decryptedCreds == -1) {
 				console.error("No decrypted creds");
 				return false;
 			}
 			return store.importCredentials(decryptedCreds);
-		}else {
+		} else {
 			decryptedCreds = decryptCreds(JSON.parse(data));
 			return store.importCredentials(decryptedCreds);
 		}
 	}
 }
 
-function renew(type, fqdn){
-	debug ("renew %j %j",  type,  fqdn);
-	
+function renew(type, fqdn) {
+	debug("renew %j %j", type, fqdn);
+
 }
 
-function revoke(fqdn){
-	
+function revoke(fqdn) {
+
 	var creds = store.search(fqdn)[0];
 	console.log("creds level %j", creds.level);
-	switch(creds.level){
-		case 'developer':
-		{
-			console.error("Revoke for developer cert is not avalible");
+	switch (creds.level) {
+		case 'developer': {
+			console.error("Revoke for developer cert is not available");
 			return;
 		}
-		case 'atom':
-		{
-			atomServices.revokeCert(fqdn, function(error, payload){
-				if(error){
+		case 'atom': {
+			atomServices.revokeCert(fqdn, function (error, payload) {
+				if (error) {
 					console.error("Error, %j", error);
 				}
 				console.log("Payload %j", payload);
 				return payload;
 			});
+			break;
 		}
-		case 'edgeclient':
-		{
+		case 'edgeclient': {
 			console.log("Calling revoke cert");
-			edgeClientServices.revokeCert(fqdn, function(error, payload){
-				if(error){
+			edgeClientServices.revokeCert(fqdn, function (error, payload) {
+				if (error) {
 					console.error("Error, %j", error);
 				}
 				console.log("Payload %j", payload);
 				return payload;
 			});
+			break;
 		}
-	};
-	debug ("revoke   %j",   fqdn);
+	}
+
+	debug("revoke   %j", fqdn);
 }
 
 function shred(fqdn, callback) {
-	if(!fqdn) {
+	if (!fqdn) {
 		throw new Error("FQDN is required in shred");
 	}
 	store.shredCredentials(fqdn, callback);
 }
 
-function stats(fqdn, format){
+function stats(fqdn){
+	if (!fqdn) {
+		throw new Error("FQDN is required in shred");
+	}
 
-	return {show: "stats"};
+	var creds = store.search(fqdn)[0];
+
+	if(!creds){
+		throw new Error("FQDN not found");
+	}
+
+	var cb = function(error,payload){
+		if(!error) return payload;
+
+		throw new Error(error.message);
+	};
+
+	switch (creds.level) {
+		case 'developer': {
+			developerServices.getStats(fqdn,cb);
+			break;
+		}
+		case 'atom': {
+			atomServices.getStats(fqdn,cb);
+			break;
+		}
+		case 'edgeclient': {
+			edgeClientServices.getStats(fqdn,cb);
+			break;
+		}
+	}
+
 }
-
 stats.toText = lineToText;
+
 module.exports = {
 	show,
 	list,
