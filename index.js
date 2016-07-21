576c7afe31c7f52753c09dc671b66b8851e93490
zenit1 <serge@beame.io>
2016-07-21 16:29:44 +0300

    index

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 module.exports = {
-    BeameCertStore: require("./src/services/BeameStore.js"),
-    BeameDirServices: require("./src/services/BeameDirServices.js"),
-    sampleNodeServer: require("./src/services/BaseHttpsServer.js"),
-    provisionApiServices: require("./src/services/ProvisionApi"),
-    developerCervices: require('./src/core/DeveloperServices'),
-    atomServices: require('./src/core/AtomServices'),
-    edgeClientServices: require('./src/core/EdgeClientServices'),
-    crypto: require('./src/cli/crypto'),
-    beameUtils: require('./src/utils/BeameUtils'),
-    socketUtils: require('./src/utils/SocketUtils'),
-    proxyClient : require("./src/services/ProxyClient")
+	sampleNodeServer: require("./src/services/BaseHttpsServer.js"),
+	crypto: require('./src/cli/crypto'),
+	proxyClient: require("./src/services/ProxyClient")
 };
