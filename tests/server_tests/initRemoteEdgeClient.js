0ae2433019521239a34531ba527987daed68028a
zenit1 <serge@beame.io>
2016-08-28 20:01:07 +0300

    remote client logging small fixes

--- tests/server_tests/initRemoteEdgeClient.js
+++ tests/server_tests/initRemoteEdgeClient.js
@@ -1,28 +1,31 @@
 var config        = require('../../config/Config');
-console.log('Trying to init first Beame remote edgeClient');
-if(config.InitFirstRemoteEdgeClient){
+const module_name = config.AppModules.BeameSDKlauncher;
+var logger        = new (require('../../src/utils/Logger'))(module_name);
 
-	var beameSdkDir = config.npmRootDir+"/lib/node_modules/beame-sdk";
+logger.info('Trying to init first Beame remote edgeClient');
 
-	var servers = require(beameSdkDir+"/src/cli/servers");
-	var creds = require(beameSdkDir+"/src/cli/creds");
+if (config.InitFirstRemoteEdgeClient) {
+
+	var beameSdkDir = config.npmRootDir + "/lib/node_modules/beame-sdk";
+
+	var servers = require(beameSdkDir + "/src/cli/servers");
+	var creds   = require(beameSdkDir + "/src/cli/creds");
 
 	var atoms         = creds.list("atom", "", "JSON");
 	var edgeclients   = creds.list("edgeclient", "", "JSON");
 	var remoteclients = creds.list("remoteclient", "", "JSON");
-	if ((remoteclients.length > 0) || (atoms.length > 0) || (edgeclients.length > 0)){
-		console.log('beame credentials found, initializing FIRST client aborted');
+	if ((remoteclients.length > 0) || (atoms.length > 0) || (edgeclients.length > 0)) {
+		logger.warn('beame credentials found, initializing FIRST client aborted');
 	}
-	else{
+	else {
 		var remoteClientServices = new (require('../../src/core/RemoteClientServices'))();
-		var BeameLogger = require('../../src/utils/Logger');
 
-		const module_name = config.AppModules.BeameSDKlauncher;
-		var logger        = new BeameLogger(module_name);
-		remoteClientServices.createEdgeClient(function(error, message){
-			if(!error){
+
+		remoteClientServices.createEdgeClient(function (error, message) {
+			if (!error) {
 				var hostname = message.hostname;
 				logger.info(`Registered new routable host: ${hostname} starting socketio chat`);
+
 				console.log("\n\n                                     x");
 				console.log("            X                                 /^^^^^^^\\               X   ");
 				console.log("    x                                /=========================\\                           x           X");
@@ -36,23 +39,28 @@ if(config.InitFirstRemoteEdgeClient){
 				console.log("              x                          \\=================/          x                           X");
 				console.log("   x                             X            \\@x@y@u@/                      X               x");
 				console.log("\n\n<@$@>\n");
-/*				var busy = false;
-				var safeCounter = 10;
-				var runMyChat = setInterval(function () {
-					if(!busy){
-						busy = true;
-						if(--safeCounter < 1)clearInterval(runMyChat);
-						try {*/
-							servers.launchChat(hostname);
-/*							clearInterval(runMyChat);
-						}
-						catch(e){
-							busy = false;
-							console.log('.x.x.x.x.x');
-						}
-					}
-				},500);*/
+				/*				var busy = false;
+				 var safeCounter = 10;
+				 var runMyChat = setInterval(function () {
+				 if(!busy){
+				 busy = true;
+				 if(--safeCounter < 1)clearInterval(runMyChat);
+				 try {*/
+				servers.launchChat(hostname);
+				/*							clearInterval(runMyChat);
+				 }
+				 catch(e){
+				 busy = false;
+				 console.log('.x.x.x.x.x');
+				 }
+				 }
+				 },500);*/
+
+				return;
 			}
+
+			console.error('Failed to load remote client server',error)
+
 		}, null, null);
 	}
 }
