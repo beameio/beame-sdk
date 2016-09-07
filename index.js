798aeb63a03f37055dbb5071ac7e0a7ac45398da
zenit1 <serge@beame.io>
2016-09-07 21:09:47 +0300

    Credentilas exported in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-    creds: require('./src/cli/creds'),
-    crypto: require('./src/cli/crypto'),
-    BaseHttpsServer: require("./src/services/BaseHttpsServer.js")
+	creds:           require('./src/cli/creds'),
+	crypto:          require('./src/cli/crypto'),
+	BaseHttpsServer: require("./src/services/BaseHttpsServer.js"),
+	Credential:      require('./src/services/Credential')
 };
