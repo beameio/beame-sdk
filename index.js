da01e871192334f8052333039ceae6126c95b5f6
zenit1 <serge@beame.io>
2016-09-07 21:11:55 +0300

    BeameStore exported in index.js instead Credentials

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
 	creds:           require('./src/cli/creds'),
 	crypto:          require('./src/cli/crypto'),
 	BaseHttpsServer: require("./src/services/BaseHttpsServer.js"),
-	Credential:      require('./src/services/Credential')
+	BeameStore:      require('./src/services/BeameStoreV2')
 };
