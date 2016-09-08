e668d4880f366b502026ba856cd5b45ffabc24ef
Ilya Sher <ilya.sher@coding-knight.com>
2016-09-08 10:40:30 +0300

    Add src/services/EC2AuthInfoCollector.js

---
+++ src/services/EC2AuthInfoCollector.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const request = require('request');
+
+const URLS = {
+	doc: 'http://169.254.169.254/latest/dynamic/instance-identity/document',
+	sig: 'http://169.254.169.254/latest/dynamic/instance-identity/pkcs7',
+}
+
+class EC2AuthInfoCollector {
+
+	/**
+	 * @returns {String}
+	 */
+	getAuthInfo() {
+		var ret = {};
+		return new Promise((resolve, reject) => {
+			var outstandingRequestsCount = Object.keys(URLS).length;
+			for(let k in URLS) {
+				request(URLS[k], (error, response, body) => {
+					if(error) {
+						reject(error);
+						return;
+					}
+					if(response.statusCode != 200) {
+						reject(`Got invalid response code for URL ${URLS[k]}`);
+						return;
+					}
+					ret[k] = body;
+					outstandingRequestsCount--;
+					if(outstandingRequestsCount == 0) {
+						ret.sig = `-----BEGIN PKCS7-----\n${ret.sig}\n-----END PKCS7-----\n`;
+						resolve(JSON.stringify(ret));
+					}
+				})
+			}
+		});
+	}
+}
+
+module.exports = {
+	EC2AuthInfoCollector
+}
