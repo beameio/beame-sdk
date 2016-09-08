78b582a5826803878d836cb64d8360591561843e
zenit1 <serge@beame.io>
2016-09-08 22:07:09 +0300

    initfromX509 pem to sync

--- src/services/Credential.js
+++ src/services/Credential.js
@@ -77,6 +77,7 @@ class Credential {
 
 
 	initFromX509(x509, metadata) {
+		pem.config({sync: true});
 		pem.readCertificateInfo(x509, (err, certData) => {
 			if (!err) {
 				this.certData           = certData ? certData : err;
@@ -91,6 +92,7 @@ class Credential {
                 this.metadata[key] = value;
             });
         }
+		pem.config({sync: false});
 	}
 
 	toJSON() {
