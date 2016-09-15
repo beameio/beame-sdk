e920bd079c68e6db64ef8528f14152ffe1597fbe
Ilya Sher <ilya.sher@coding-knight.com>
2016-09-15 16:47:50 +0300

    WIP: tests/cryptoServer.js and tests/cryptoClient.js for iOS tests

--- tests/cryptoClient.js
+++ tests/cryptoClient.js
@@ -2,4 +2,19 @@
 
 const io		= require('socket.io-client')('http://127.0.0.1:65000/');
 
-io.emit('event', {"type": "key", "payload": "_publicKeyB64_"});
+const store2    = new (require("../src/services/BeameStoreV2"))();
+const fqdn		= process.argv[2];
+
+if(!fqdn) {
+	throw new Error('Must supply command line positional parameter fqdn');
+}
+
+const creds     = store2.getCredential(fqdn);
+
+const pubKeyLines = creds.publicKeyStr.split('\n');
+
+const pubKeyDerBase64 = pubKeyLines.slice(1, pubKeyLines.length-1).join('\n');
+
+// exampleSocket.send(JSON.stringify({'type':'key','payload':arrayBufferToBase64String(keyPair.publicKey)}));
+// exampleSocket.send(JSON.stringify({'type':'key','payload':{'key':arrayBufferToBase64String(keyPair.publicKey), 'token': {'signedData':'key','signedBy':'signedBy','signature':'signature'}}}));
+io.emit('event', {type: 'key', payload: {key: pubKeyDerBase64}});
--- tests/cryptoServer.js
+++ tests/cryptoServer.js
@@ -1,10 +1,7 @@
 "use strict";
 
 const io		= require('socket.io')();
-
-const store2    = new (require("../src/services/BeameStoreV2"))();
 const tunnel    = require('../src/cli/tunnel');
-
 const fqdn		= process.argv[2];
 
 // SOCKET_MSG_TYPES[] = { @"key", @"signkey",@"data",@"symdata",@"symdatawithiv",@"cert"};
@@ -21,9 +18,12 @@ console.log(`Using local port ${io.httpServer.address().port}`);
 // console.log(`Will create beame tunnel using fqdn ${fqdn} to forward traffic to local SocketIO server`);
 // tunnel.httpsTunnel(fqdn, '127.0.0.1', io.httpServer.address().port, 'http');
 
+var peerPubKey = null;
+
 const handlers = {
 	key(data) {
-		console.log('KEY PAYLOAD', data);
+		console.log('KEY PAYLOAD %j', data);
+		peerPubKey = data.key;
 	}
 }
 
@@ -31,6 +31,7 @@ io.on('connection', client => {
 	console.log('SocketIO connection');
 	// console.log('Received SocketIO message %j', msg);
 	client.on('event', data => {
+		var result;
 		console.log('SocketIO event: %j', data);
 		if(!data.type) {
 			console.error('Data has no type field');
@@ -41,7 +42,6 @@ io.on('connection', client => {
 			return;
 		}
 		result = handlers[data.type](data.payload);
-			
+		console.log('SocketIO result: %j', result);
 	});
-
 });
