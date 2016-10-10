4a65e6aa502c43eae5ba6c12818b3e53c6f383af
Ilya Sher <ilya.sher@coding-knight.com>
2016-10-10 11:29:41 +0300

    [CLI] Improve aesEncrypt and aesDecrypt

--- src/cli/crypto.js
+++ src/cli/crypto.js
@@ -7,6 +7,16 @@ const CommonUtils    = require('../utils/CommonUtils');
 require('../../initWin');
 
 /**
+ *
+ * @param o
+ * @returns {String|*|string}
+ * @private
+ */
+function _obj2base64(o) {
+	return Buffer(CommonUtils.stringify(o, false)).toString('base64');
+}
+
+/**
  * @public
  * @method Crypto.aesEncrypt
  * @param {String} data
@@ -14,42 +24,23 @@ require('../../initWin');
  * @returns string
  */
 function aesEncrypt(data, secret) {
-
 	let sharedSecret = secret ? new Buffer(secret, 'base64') : null;
-
-	let array = CryptoServices.aesEncrypt(data, sharedSecret);
-
-	let token ={};
-
-	array.forEach((el,ind)=> {
-
-		token[ind] = el;
-	});
-
-	return new Buffer(CommonUtils.stringify(token, false)).toString('base64');
-
+	return CryptoServices.aesEncrypt(data, sharedSecret);
 }
+
+aesEncrypt.toText = _obj2base64;
 
 /**
  * @public
  * @method Crypto.aesDecrypt
- * @param {String} data
+ * @param {Array} encryptedData
  * @returns {String}
  */
-function aesDecrypt(data) {
-
-	var token   = CommonUtils.parse(new Buffer(data, 'base64').toString());
-
-	let array =[];
-
-	for(let key in token){
-		if(token.hasOwnProperty(key)){
-			array.push(token[key]);
-		}
-	}
-
-	return CryptoServices.aesDecrypt(array);
+function aesDecrypt(encryptedData) {
+	return CryptoServices.aesDecrypt(encryptedData);
 }
+
+aesDecrypt.toText = x => x;
 
 module.exports = {
 	aesEncrypt,
