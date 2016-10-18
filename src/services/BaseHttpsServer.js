0c95e41322fa29e2a145db3b58bf56cf84a5728a
zenit1 <serge@beame.io>
2016-10-18 13:05:17 +0300

    jsdoc

--- src/services/BaseHttpsServer.js
+++ src/services/BaseHttpsServer.js
@@ -19,7 +19,7 @@ const logger      = new (require('../utils/Logger'))(config.AppModules.BaseHttps
  * @param {String} fqdn - fqdn of the HTTPS server. You must have private key of the entity.
  * @param {Function} requestListener - requestListener parameter for https.createServer(), express application for example
  * @param {Function|null} [hostOnlineCallback]
- * @param {Object|null} [options]
+ * @param {Object|null} [options] => https server options
  */
 function BaseBeameHttpsServer(fqdn, options, requestListener, hostOnlineCallback) {
 
