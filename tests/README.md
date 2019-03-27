4c1c90e5922173ff3b2131071fac214afdbea1af
Ricardo Gomes <ricardo@heptasoft.com>
2019-03-27 17:51:56 +0100

    Update dependencies. Split of credential and custom credential tests. Version increment to prepare for release.

--- tests/README.md
+++ tests/README.md
@@ -27,6 +27,12 @@ Tests the Common Utils.
 Tests on the credentials functionality
 
 Requires `BEAME_TESTS_LOCAL_ROOT_FQDN` (fqdn of a local root available cred)
+
+### tests_custom_credentials.js (requires special credentials)
+Tests on the creation of custom credentials.
+This test requires a special level of credentials
+
+Requires `BEAME_TESTS_LOCAL_ROOT_FQDN` (fqdn of a local root available cred)
 Optionally `BEAME_TESTS_CUSTOM_FQDN` can be passed for a custom fqdn creation in the custom fqdn test.
 
 ### test_ntp.js
@@ -41,3 +47,16 @@ Tests in the oscp functionality
 Requires `BEAME_TESTS_LOCAL_FQDN` (fqdn of a local available cred) in order to run the ocsp tests againts.
 
 Sets internally `EXTERNAL_OCSP_FQDN` and `BEAME_OCSP_IGNORE` as required to test the functionality
+
+## Running examples
+
+From the tests folder run:
+
+`BEAME_TESTS_LOCAL_ROOT_FQDN=jadtndigzadmevz7.v1.d.beameio.net BEAME_ENV=dev ../node_modules/mocha/bin/mocha unit_tests/test_credentials.js`
+
+`BEAME_TESTS_LOCAL_FQDN=jadtndigzadmevz7.v1.d.beameio.net BEAME_ENV=dev ../node_modules/mocha/bin/mocha unit_tests/test_ocsp.js`
+
+
+For debugging information on the tests, add the `DEBUG=beame:sdk:unittests:*`
+
+In production environment, just use a `.p.beameio.net` credential and remove the `BEAME_ENV=dev`.
--- tests/unit_tests/test_credentials.js
+++ tests/unit_tests/test_credentials.js
@@ -15,7 +15,7 @@ function _getRandomRegistrationData(prefix) {
 }
 
 if(!process.env.BEAME_TESTS_LOCAL_ROOT_FQDN)
-	throw "Env BEAME_TESTS_LOCAL_ROOT_FQDN is required";
+	throw new Error("Env BEAME_TESTS_LOCAL_ROOT_FQDN is required");
 
 describe('local_creds_create', function () {
 	this.timeout(100000);
@@ -45,44 +45,6 @@ describe('local_creds_create', function () {
 			done();
 		}).catch(error=> {
 			debug(error);
-			assert.fail(error);
-			done();
-		});
-	});
-
-});
-
-describe('local_creds_custom_create', function () {
-	this.timeout(100000);
-	const rnd = beameUtils.randomString(8).toLowerCase();
-
-	let parent_fqdn = process.env.BEAME_TESTS_LOCAL_ROOT_FQDN;
-	let custom_fqdn = process.env.BEAME_TESTS_CUSTOM_FQDN || `c-${rnd}.tests`;
-	let data = _getRandomRegistrationData(`${parent_fqdn}-child-`);
-	debug('*** createCustomWithLocalCreds data %j', data);
-	debug('*** createCustomWithLocalCreds parent_fqdn=%s custom_fqdn=%s', parent_fqdn, custom_fqdn);
-	let parent_cred;
-
-	before(function () {
-		assert(parent_fqdn, 'Parent fqdn required');
-		debug('find local creds');
-		parent_cred = store.getCredential(parent_fqdn);
-		assert(parent_cred, 'Parent credential not found');
-	});
-
-	it('Should create entity', function (done) {
-		parent_cred.createCustomEntityWithLocalCreds(parent_fqdn, custom_fqdn, data.name, data.email).then(metadata => {
-			debug('metadata received %j', metadata);
-			assert(metadata, `expected metadata`);
-			assert(metadata.fqdn, `expected fqdn`);
-
-			let cred = store.getCredential(metadata.fqdn);
-			assert(cred, 'New credential not found inn store');
-
-			done();
-		}).catch(error=> {
-			debug(error);
-			debug(error.EntityValidationErrors);
 			assert.fail(error);
 			done();
 		});
