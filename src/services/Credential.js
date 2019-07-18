9141be2c2b3a964d81b2313b3c3a42bb1b257ab4
Ricardo Gomes <ricardo@heptasoft.com>
2019-07-18 12:50:45 +0200

    Added todo

--- src/services/Credential.js
+++ src/services/Credential.js
@@ -2689,6 +2689,7 @@ class Credential {
 		return false;
 	}
 
+	// TODO: Ric - Fix logic, it seems broken
 	get expired() {
 		try {
 			return CommonUtils.isObjectEmpty(this.certData) ? true : new Date(this.certData.validity.end) < new Date();
